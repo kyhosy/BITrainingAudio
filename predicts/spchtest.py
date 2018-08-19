89a72106e9d837f3c0bdf68d409cb70dd2204b3b
kyho <ky.ho@unicorn.vn>
2018-08-19 16:45:52 +0700

    improve training data

--- predicts/spchtest.py
+++ predicts/spchtest.py
@@ -24,28 +24,39 @@ def get_MFCC(sr,audio):
     features = preprocessing.scale(features)
     return features
 
-#path to testing data
-sourcepath = "pygender\\pygender\\test_data\\AudioSet\\female_clips\\"
-#path to saved models
-modelpath  = "pygender\\pygender\\"
+def test(gender):
+    print('BEGIN DETECT: {}'.format(gender))
+    #path to testing data
+    sourcepath = "C:\\Users\\dell\\PycharmProjects\\BITrainingAudio\\sources\\test_data\\{}\\".format(gender)
+    #path to saved models
+    modelpath  = "C:\\Users\\dell\\PycharmProjects\\BITrainingAudio\\sources\\model\\"
 
-gmm_files = [os.path.join(modelpath,fname) for fname in
-              os.listdir(modelpath) if fname.endswith('.gmm')]
-models    = [cPickle.load(open(fname,'rb')) for fname in gmm_files]
-genders   = [fname.split("\\")[-1].split(".gmm")[0] for fname
-              in gmm_files]
-files     = [os.path.join(sourcepath,f) for f in os.listdir(sourcepath)
-              if f.endswith(".wav")]
 
-for f in files:
-    print(f.split("\\")[-1])
-    sr, audio  = read(f)
-    features   = get_MFCC(sr,audio)
-    scores     = None
-    log_likelihood = np.zeros(len(models))
-    for i in range(len(models)):
-        gmm    = models[i]         #checking with each model one by one
-        scores = np.array(gmm.score(features))
-        log_likelihood[i] = scores.sum()
-    winner = np.argmax(log_likelihood)
-print("\tdetected as - ", genders[winner],"\n\tscores:female ",log_likelihood[0],",male ", log_likelihood[1],"\n")
+    gmm_files = [os.path.join(modelpath,fname) for fname in
+                  os.listdir(modelpath) if fname.endswith('.gmm')]
+    models    = [cPickle.load(open(fname,'rb')) for fname in gmm_files]
+    genders   = [fname.split("\\")[-1].split(".gmm")[0] for fname
+                  in gmm_files]
+    # print(genders)
+    files     = [os.path.join(sourcepath,f) for f in os.listdir(sourcepath)
+                  if f.endswith(".wav")]
+
+
+
+
+    for f in files:
+        print(f.split("\\")[-1])
+        sr, audio  = read(f)
+        features   = get_MFCC(sr,audio)
+        scores     = None
+        log_likelihood = np.zeros(len(models))
+        for i in range(len(models)):
+            gmm    = models[i]         #checking with each model one by one
+            scores = np.array(gmm.score(features))
+            log_likelihood[i] = scores.sum()
+        winner = np.argmax(log_likelihood)
+
+        # print("winner>>>",winner,log_likelihood)
+        print("detected as - ", genders[winner],"scores:female ",log_likelihood[0],",male ", log_likelihood[1],"\n")
+test('male')
+test('female')
--- predicts/spchtrain.py
+++ predicts/spchtrain.py
@@ -14,27 +14,36 @@ def get_MFCC(sr,audio):
     features = mfcc.mfcc(audio,sr, 0.025, 0.01, 13,appendEnergy = False)
     features = preprocessing.scale(features)
     return features
-
+# #path to training data
+# source   = "D:\\pygender\\train_data\\youtube\\male\\"
+# #path to save trained model
+# dest     = "D:\\pygender\\"
 #path to training data
-source   = "put\\your\\train_data_location"
-#path to save trained model
-dest     = "put\\trained\\model_destination"
-files    = [os.path.join(source,f) for f in os.listdir(source) if
-             f.endswith('.wav')]
-features = np.asarray(());
+# source   = "sources/train_data/male/_4oUfgt6Fzo_100-124-Male_0.wav"
+def training(gender):
+    source = "C:\\Users\\dell\\PycharmProjects\\BITrainingAudio\\sources\\train_data\\{}\\".format(gender)
+    #path to save trained model
+    dest = "C:\\Users\\dell\\PycharmProjects\\BITrainingAudio\\sources\\model\\"
+    files    = [os.path.join(source,f) for f in os.listdir(source) if
+                 f.endswith('.wav')]
+    features = np.asarray(());
 
-for f in files:
-    sr,audio = read(f)
-    vector   = get_MFCC(sr,audio)
-    if features.size == 0:
-        features = vector
-    else:
-        features = np.vstack((features, vector))
+    for f in files:
+        sr,audio = read(f)
+        vector   = get_MFCC(sr,audio)
+        print(vector)
+        if features.size == 0:
+            features = vector
+        else:
+            features = np.vstack((features, vector))
 
-gmm = GMM(n_components = 8, n_iter = 200, covariance_type='diag',
-        n_init = 3)
-gmm.fit(features)
-picklefile = f.split("\\")[-2].split(".wav")[0]+".gmm"
+    gmm = GMM(n_components = 8, n_iter = 200, covariance_type='diag',
+            n_init = 3)
+    gmm.fit(features)
+    picklefile = f.split("\\")[-2].split(".wav")[0]+".gmm"
+    print(dest + picklefile)
 
-# model saved as .gmm
-cPickle.dump(gmm,open(dest + picklefile,'wb'))
+    # model saved as .gmm
+    cPickle.dump(gmm,open(dest + picklefile,'wb'))
+training('male')
+training('female')
